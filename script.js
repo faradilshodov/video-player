039e39974d98c06c7320ba71d880a2a876529b8c
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-05-10 12:56:57 -0400

    Add seeking via progress bar

--- script.js
+++ script.js
@@ -45,6 +45,12 @@ function updateProgress() {
     duration.textContent = `${displayTime(video.duration)}`;
 }
 
+// Click to seek within the video
+function setProgress(e) {
+    const newTime = e.offsetX / progressRange.offsetWidth;
+    progressBar.style.width = `${newTime * 100}%`;
+    video.currentTime = newTime * video.duration;
+}
 
 // Volume Controls --------------------------- //
 
@@ -61,3 +67,4 @@ playBtn.addEventListener('click', togglePlay);
 video.addEventListener('click', togglePlay);
 video.addEventListener('timeupdate', updateProgress);
 video.addEventListener('canplay', updateProgress);
+progressRange.addEventListener('click', setProgress);
