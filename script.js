750c5a2248449bcabc53a93cadd9d51e26a7337a
Farrukh(Fara) Dilshodov <farrukh.dilshodov@gmail.com>
2025-05-11 15:29:16 -0400

    Added changing playback speed

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ const volumeBar = document.querySelector(".volume-bar");
 const currentTime = document.querySelector(".time-elapsed");
 const duration = document.querySelector(".time-duration");
 const fullscreenBtn = document.querySelector(".fullscreen");
+const speed = document.querySelector(".player-speed");
 
 // Play & Pause ----------------------------------- //
 function showPlayIcon() {
@@ -98,6 +99,9 @@ function toggleMute() {
 }
 
 // Change Playback Speed -------------------- //
+function changeSpeed() {
+    video.playbackRate = speed.value;
+}
 
 // Fullscreen ------------------------------- //
 
@@ -109,3 +113,4 @@ video.addEventListener("canplay", updateProgress);
 progressRange.addEventListener("click", setProgress);
 volumeRange.addEventListener("click", changeVolume);
 volumeIcon.addEventListener("click", toggleMute);
+speed.addEventListener("change", changeSpeed);
